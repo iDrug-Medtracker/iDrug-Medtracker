faca6a7ed3c90eba9d5a4af059e1b76598a1b3f4
eric8 <eric82900@gmail.com>
2020-02-07 16:32:21 -0500

    added functionality to NewPillActivity created medication class medications stored in array array stored in sharedPreferences so it is not lost when closing app array can be used across activities

--- app/src/main/java/com/example/idrug01/MainActivity.java
+++ app/src/main/java/com/example/idrug01/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.idrug01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,31 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private int progressStatus = 0;
     private ImageButton button1;
+    ArrayList<medication> medList;
 
-    //not used, example function for eric
-    public static int extractHourOfDayFromMS(long ms) {
-        int millis = (int) (ms % 86400000);
-
-        if (millis < 0) {
-            millis += 86400000;
-        }
-        while (millis < 0) {
-            millis += 86400000;
-        }
-        while (millis >= 86400000) {
-            millis -= 86400000;
-        }
-        millis /= (1000*60*60);
-        return millis % 24;
-    }
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //load medList with function created bellow
+        loadData();
 
-        button1 = (ImageButton) findViewById(R.id.buttonnewpill);
+        button1 = findViewById(R.id.buttonnewpill);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        progressBar = (ProgressBar) findViewById(R.id.circulo);
+        progressBar = findViewById(R.id.circulo);
         //to get a clock
         Thread t = new Thread() {
             @Override
@@ -61,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                TextView tdate = (TextView) findViewById(R.id.date);
+                                TextView tdate = findViewById(R.id.date);
                                 long date = System.currentTimeMillis();
                                 SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
                                 String dateString = sdf.format(date);
@@ -87,5 +79,17 @@ public class MainActivity extends AppCompatActivity {
     public void openNewPillForm(){
         Intent intent = new Intent(this, NewPillActivity.class);
         startActivity(intent);
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<medication>>() {}.getType();
+        medList = gson.fromJson(json, type);
+
+        if (medList == null) {
+            medList = new ArrayList<>();
+        }
     }
 }
--- app/src/main/java/com/example/idrug01/NewPillActivity.java
+++ app/src/main/java/com/example/idrug01/NewPillActivity.java
@@ -2,13 +2,107 @@ package com.example.idrug01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class NewPillActivity extends AppCompatActivity {
+    ArrayList<medication> medList;
+    private String name;
+    private String times;
+    private int dosage;
+    private int period;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_pill_form);
+        //load medList with function created bellow
+        loadData();
+
+        //just to see if medList is working display all its content
+        TextView DisplayStringArray = findViewById(R.id.lista);
+        DisplayStringArray.setTextSize(15);
+        for (int i=0; i<medList.size();i++) {
+            DisplayStringArray.append(medList.get(i).name);
+            DisplayStringArray.append(" ");
+            DisplayStringArray.append(medList.get(i).times);
+            DisplayStringArray.append(" ");
+            DisplayStringArray.append(String.valueOf(medList.get(i).dosage));
+            DisplayStringArray.append(" ");
+            DisplayStringArray.append(String.valueOf(medList.get(i).period));
+            DisplayStringArray.append("\n");
+
+        }
+
+        //buttonDone should add the new medication and save ArrayList
+        Button buttonDone = findViewById(R.id.but_done);
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et1 = findViewById(R.id.editName);
+                name = et1.getEditableText().toString();
+                EditText et2 = findViewById(R.id.editTimes);
+                times = et2.getEditableText().toString();
+                EditText et3 = findViewById(R.id.editDosage);
+                //check fot integers or it crashes
+                try {
+                    dosage = Integer.parseInt(et3.getEditableText().toString());
+                } catch (NumberFormatException e) {
+                    dosage = 0;
+                }
+                EditText et4 = findViewById(R.id.editPeriod);
+                try {
+                    period = Integer.parseInt(et4.getEditableText().toString());
+                } catch (NumberFormatException e) {
+                    period = 0;
+                }
+                medList.add( new medication(name, times, dosage, period));
+                saveData();
+                openHome();
+            }
+        });
+        Button buttonReset = findViewById(R.id.reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                medList.clear();
+                saveData();
+            }
+        });
+    }
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(medList);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<medication>>() {}.getType();
+        medList = gson.fromJson(json, type);
+        if (medList == null) {
+            medList = new ArrayList<>();
+        }
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
